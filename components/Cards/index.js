2ac1ccc7bf54739a4163b5fbab1407a1c81b2033
Zach Lasky <zacharylasky123@gmail.com>
2019-07-13 12:46:07 -0400

    connected to pictures style

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -5,17 +5,6 @@
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
-//
-// <div class="card">
-//   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {authors name}</span>
-//   </div>
-// </div>
-//
 // Create a card for each of the articles and add the card to the DOM.
 
 const cardsContainer = document.querySelector(".cards-container");
@@ -46,16 +35,25 @@ function CardsComponents(object) {
     name = document.createElement("span");
   //FILL IN CONTENT
   headline.textContent = object.headline;
-  // img.src = object.authorPhoto;
+  img.src = object.authorPhoto;
   name.textContent = object.authorName;
   //APPEND CHILDREN
-  card.append(headline, author, imgContainer, name);
-  author.append(imgContainer);
+  card.append(headline, author, imgContainer);
+  author.append(imgContainer, name);
   imgContainer.append(img);
-  author.append(name);
   //STYLE ELEMENTS
   card.classList.add("card");
   author.classList.add("author");
-
+  imgContainer.classList.add("img-container");
   return card;
 }
+
+// <div class="card">
+//   <div class="headline">{Headline of article}</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={url of authors image} />
+//     </div>
+//     <span>By {authors name}</span>
+//   </div>
+// </div>
